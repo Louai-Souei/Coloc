fd6ae030bc84ba572b692322747511156567ece5
hendlegleg <hendlegleg1@gmail.com>
2024-12-29 22:22:39 +0100

    commit_299

--- src/main/java/CU/projet/ColocationUniversitaire/config/CorsConfig.java
+++ src/main/java/CU/projet/ColocationUniversitaire/config/CorsConfig.java
@@ -1,4 +1,36 @@
 package CU.projet.ColocationUniversitaire.config;
 
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
 public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                log.info("Configuring CORS settings...");
+
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods(HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.DELETE.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.PATCH.name())
+                        .allowedHeaders(HttpHeaders.CONTENT_TYPE,
+                                HttpHeaders.AUTHORIZATION);
+
+                log.info("CORS settings configured successfully.");
+            }
+        };
+    }
 }
